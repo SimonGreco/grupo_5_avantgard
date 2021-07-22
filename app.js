cef92406d4ab9c6039cec2a931f47890368b93cc
SimonGreco <simongreco60@gmail.com>
2021-07-21 21:55:43 -0300

    app

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static("public"));
+
+app.listen(3000, function(){
+    console.log("servidor levantado en el puerto 3000")
+});
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/home.html"))
+});
