3b285d76889a3352642f9a87809e4d45064f868f
SimonGreco <simongreco60@gmail.com>
2021-07-22 00:11:17 -0300

    header y footer

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.listen(3000, function(){
     console.log("servidor levantado en el puerto 3000")
 });
 
-app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/home.html"))
-});
+// app.get("/", function(req, res){
+//     res.sendFile(path.join(__dirname, "/views/home.html"))
+// });
