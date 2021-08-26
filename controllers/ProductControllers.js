3c9a454de4565abf56c3be7d183c397700820213
SimonGreco <simongreco60@gmail.com>
2021-08-26 20:36:39 -0300

    f

--- controllers/ProductControllers.js
+++ controllers/ProductControllers.js
@@ -23,24 +23,24 @@ let productController = {
         res.render("./products/productCtlg", {productos: productos})
     },
     create: function(req,res){
-        // let productoNuevo = {
-        //     name: req.body.name,
-        //     id: Date.now(),
-        //     image: req.body.image,
-        //     price: req.body.price,
-        //     categoria: req.body.categoria,
-        //     estado: req.body.estado,
-        //     marca: req.body.marca,
-        //     modelo: req.body.modelo,
-        //     description: req.body.description,
-        //     Oferta: req.body.Oferta,
-        // }
-        // let newJSON = productos.concat(productoNuevo);
-		// let productJSON = JSON.stringify(newJSON, null, 2);
+        let productoNuevo = {
+            name: req.body.name,
+            id: Date.now(),
+            image: req.body.image,
+            price: req.body.price,
+            categoria: req.body.categoria,
+            estado: req.body.estado,
+            marca: req.body.marca,
+            modelo: req.body.modelo,
+            description: req.body.description,
+            Oferta: req.body.Oferta,
+        }
+        let newJSON = productos.concat(productoNuevo);
+		let productJSON = JSON.stringify(newJSON, null, 2);
 
-		// fs.writeFileSync( productsFilePath, productJSON);
-        // res.redirect("/products/")
-        res.send(req.files)
+		fs.writeFileSync( productsFilePath, productJSON);
+        res.redirect("/products/")
+        
     }
 
 }
