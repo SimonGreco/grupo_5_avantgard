6b1602d983cef1551c79c9b883459dfb01a8b7dc
SimonGreco <simongreco60@gmail.com>
2021-10-20 23:33:34 -0300

    db

--- avantGard_db.sql
+++ avantGard_db.sql
@@ -1,96 +1,142 @@
-CREATE DATABASE AvantGard /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+CREATE DATABASE  IF NOT EXISTS `avantgard` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `avantgard`;
+-- MySQL dump 10.13  Distrib 5.7.24, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: avantgard
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.4.21-MariaDB
 
--- cities definition
-
---  Drop table
-
---  DROP TABLE cities;
-
-CREATE TABLE cities (
-	id int(11) auto_increment NOT NULL,
-	name varchar(45) NULL,
-	CONSTRAINT id_PK PRIMARY KEY (id),
-	CONSTRAINT id_UN UNIQUE KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS cities;
-
-
--- categories definition
-
---  Drop table
-
---  DROP TABLE categories;
-
-CREATE TABLE categories (
-	id INT(11) auto_increment NULL,
-	name varchar(45) NULL,
-	CONSTRAINT id_PK PRIMARY KEY (id),
-	CONSTRAINT categories_UN UNIQUE KEY (id, name)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS categories;
-
-
--- products definition
-
---  Drop table
-
---  DROP TABLE products;
-
-CREATE TABLE products (
-	id INT(11) auto_increment NULL,
-	name varchar(100) NOT NULL,
-	price varchar(100) NOT NULL,
-	estado varchar(100) NULL,
-	marca VARCHAR(100) NULL,
-	modelo VARCHAR(100) NULL,
-	description TEXT NULL,
-	image BLOB NULL,
-	oferta BOOL NULL,
-	categoria_Id INT NOT NULL,
-	usuario_Id INT NOT NULL,
-	CONSTRAINT id_PK PRIMARY KEY (id),
-	CONSTRAINT id_UN UNIQUE KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS products;
-
-
--- users definition
-
---  Drop table
-
---  DROP TABLE users;
-
-CREATE TABLE users (
-	id INT(11) auto_increment NULL,
-	firstName varchar(100) NOT NULL,
-	lastName varchar(100) NOT NULL,
-	email varchar(100) NOT NULL,
-	password varchar(255) NOT NULL,
-	admin BOOL NOT NULL,
-	document INT(8) NOT NULL,
-	phone INT(11) NULL,
-	adress varchar(100) NULL,
-	postCode INT NULL,
-	CONSTRAINT id_PK PRIMARY KEY (id),
-	CONSTRAINT users_UN UNIQUE KEY (id,email)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;
-DROP TABLE IF EXISTS users;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping routines for database 'AvantGard'
+-- Table structure for table `categories`
 --
+
+DROP TABLE IF EXISTS `categories`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categories`
+--
+
+LOCK TABLES `categories` WRITE;
+/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cities`
+--
+
+DROP TABLE IF EXISTS `cities`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `cities` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cities`
+--
+
+LOCK TABLES `cities` WRITE;
+/*!40000 ALTER TABLE `cities` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cities` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `price` varchar(45) NOT NULL,
+  `estado` varchar(45) DEFAULT NULL,
+  `marca` varchar(45) DEFAULT NULL,
+  `modelo` varchar(45) DEFAULT NULL,
+  `description` text NOT NULL,
+  `image` varchar(100) DEFAULT NULL,
+  `oferta` tinyint(4) NOT NULL,
+  `categoria_id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `usuario_id_idx` (`usuario_id`),
+  KEY `categoria_id_idx` (`categoria_id`),
+  CONSTRAINT `categoria_id` FOREIGN KEY (`categoria_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `usuario_id` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `products`
+--
+
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `admin` tinyint(4) NOT NULL,
+  `image` varchar(200) DEFAULT NULL,
+  `documento` int(11) DEFAULT NULL,
+  `phone` int(11) DEFAULT NULL,
+  `addres` varchar(100) DEFAULT NULL,
+  `ciudad_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `ciudad_id_idx` (`ciudad_id`),
+  CONSTRAINT `ciudad_id` FOREIGN KEY (`ciudad_id`) REFERENCES `cities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -101,4 +147,4 @@ DROP TABLE IF EXISTS users;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-20  0:00:00
+-- Dump completed on 2021-10-20 23:32:00
