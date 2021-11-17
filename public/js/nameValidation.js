6cbf9d8c856b4b21a5d7fbd5003f1229ca559526
reeeckeee <munozpedro2002@gmail.com>
2021-11-17 20:32:53 -0300

    validaciones-front

--- public/js/nameValidation.js
+++ public/js/nameValidation.js
@@ -1,11 +1,2 @@
-window.addEventListener("load", function() {
-    let title = document.querySelector("input.title");
-    title.addEventListener("submit", function(e) {
-        e.preventDefault;
-        if (title.value == "") {
-            alert("Este campo es obligatorio.")
-        } else if (title.value.length <= 5) {
-            alert("Este campo debe tener al menos 5 caracteres.")
-        }
-    });
-})
+window.addEventListener('load', function() {
+    let body = document.querySelector("body").style.backgroundColor = "lightskyblue"})    
