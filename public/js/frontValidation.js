c0f3cbd9ce6cb10bf7939251d7940e531121b5a2
reeeckeee <munozpedro2002@gmail.com>
2021-11-19 15:41:45 -0300

    front-validation

--- public/js/frontValidation.js
+++ public/js/frontValidation.js
@@ -1,2 +1,29 @@
-window.addEventListener('load', function() {
-    let body = document.querySelector("body").style.backgroundColor = "lightskyblue"})    
+window.addEventListener("load", function() {
+    let formulario = document.querySelector("form.creationForm")
+
+    formulario.addEventListener("submit", function(e) {
+        e.preventDefault();
+
+        let putImage = document.querySelector("input.addImage");
+
+        if (putImage.file != JPG, JPEG, PNG, GIF) {
+            alert("El formato del archivo debe ser válido (JPG, JPEG, PNG, GIF)");
+        }
+
+        
+        let putTitle = document.querySelector("input.formulario-txt");
+
+        if (putTitle.value == "") {
+            alert("Este campo es obligatorio");
+        } else if (putTitle.value.length < 5) {
+            alert("El título debe tener al menos 5 caracteres");
+        }
+
+
+        let  putDescription = document.querySelector("textarea.Descripcion");
+
+        if (putDescription.value.length < 20) {
+            alert("La descripción debe tener al menos 20 caracteres");
+        }
+    });
+});      
