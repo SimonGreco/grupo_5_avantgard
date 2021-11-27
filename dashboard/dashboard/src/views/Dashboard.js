78c98414eff57389b5b3bc32b2cf5436545d2a65
SimonGreco <simongreco60@gmail.com>
2021-11-27 19:09:44 -0300

    vista dashboard funcionando y navbar

--- dashboard/dashboard/src/views/Dashboard.js
+++ dashboard/dashboard/src/views/Dashboard.js
@@ -1,10 +1,27 @@
 import React from "react";
+import {Grid} from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles'
+import Navbar from '../components/Navbar' 
+import 'fontsource-montserrat'
 
+
+const useStyles = makeStyles (()=>({
+    root : {
+        flexGrow : 1
+    }
+}))
 
 function Dashboard(props){
+    const classes = useStyles()
     return(
-        <div>
-           HOLA
+        <div className={classes.root}>
+           <Grid container spacing={3}>
+           
+            <Grid item xs={12}>
+            <Navbar/>
+            </Grid>
+
+           </Grid>
         </div>
     );
 }
