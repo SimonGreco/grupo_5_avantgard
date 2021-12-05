64717c6c55c7588e541bad66735a5bff18d7868c
reeeckeee <munozpedro2002@gmail.com>
2021-12-05 16:46:43 -0300

    front-validation

--- public/js/frontValidation.js
+++ public/js/frontValidation.js
@@ -1,4 +1,34 @@
 window.addEventListener("load", function () {
+    var putTitle = document.querySelector("input.formulario-txt");
+    var putDescription = document.querySelector("textarea.Descripcion");
+    let putImage = document.querySelector("input.addImage");
+
+    putTitle.addEventListener("change", function (e) {
+        e.preventDefault();
+        if (putTitle.value == "") {
+            alert("El nombre del producto es obligatorio");
+        } else if (putTitle.value.length < 5) {
+            alert("El título debe tener al menos 5 caracteres");
+        }
+    })
+    
+    putDescription.addEventListener("change", function (e) {
+        e.preventDefault();
+        if (putDescription.value.length < 20) {
+            alert("La descripción debe tener al menos 20 caracteres y no puede estar vacia");
+        }
+    })
+
+    putImage.addEventListener("change", function (e) {
+        e.preventDefault();
+        if (putImage.file != "JPG", "JPEG", "PNG") {
+            alert("El formato del archivo debe ser válido (JPG, JPEG, PNG)");
+        }})
+});
+
+
+
+/*window.addEventListener("load", function () {
     let formulario = document.querySelector("form.productsForm")
 
 
@@ -6,11 +36,13 @@ window.addEventListener("load", function () {
         e.preventDefault();
 
 
-        // let putImage = document.querySelector("input.addImage");
-
-        // let acceptedExt = [".png", ".jpg", ".jpeg", ".gif"]
-        // let extension = (path.extname(file)).toLowerCase();
-        // if(!acceptedExt.includes(extension))
+        let putImage = document.querySelector("input.addImage");
+        
+        putImage.addEventListener("change", function (e) {
+            e.preventDefault();
+            if (pImage.file != "JPG", "JPEG", "PNG") {
+                alert("El formato del archivo debe ser válido (JPG, JPEG, PNG)");
+            }})
 
 
         let putTitle = document.querySelector("input.formulario-txt");
@@ -28,4 +60,4 @@ window.addEventListener("load", function () {
             alert("La descripción debe tener al menos 20 caracteres");
         }
     });
-});
+});*/
